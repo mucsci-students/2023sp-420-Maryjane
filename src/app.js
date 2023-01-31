796c9cfe20ec750161576ef9b9c48a9732d9e5d2
mjlewis-millersville <mjlewis@millersville.edu>
2023-01-31 18:40:42 -0500

    Added vorpal as a dependency. Initialized vorpal input stream. Added example CLI command to test Vorpal.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 /* Entry point of the program defined by package.json */
 
-function main() {
-    console.log("hello world");
-}
+var vorpal = require('vorpal')();
 
-// Invokes the main method
-if (require.main === module) {
-    main();
-}
+//Initializes the CLI input stream and changes the text to show 
+//custom text.
+vorpal
+  .delimiter('SpellingBee>')
+  .show();
+
+//An example custom vorpal command that uses 'duck' as the input text
+//and outputs 'Wabbit' as the response.
+vorpal
+      .command('duck', 'Outputs "rabbit"')
+      .action(function(args, callback) {
+        this.log('Wabbit');
+        callback();
+      });
+
+// ======  Commented out due to breaking added functionality.======
+// function main() {
+//     console.log("hello there");
+    
+// }
+
+// //Invokes the main method
+// if (require.main === module) {
+//     main();
+// }
