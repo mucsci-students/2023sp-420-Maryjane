20bdc7540bf5ced93e9234be6865b91be4883234
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-02-19 13:33:12 -0500

    Added Buttons and Comments

--- src/views/GUI_View.js
+++ src/views/GUI_View.js
@@ -1,2 +1,4 @@
 //TODO!
 //Function to find a specifc html tasks by adding an ID for each tasks
+
+this.TopLeftBlock = document.getElementById(""); //Return the ID of element as a java script object, store all in the array and suffle and change what they say inside them This.TopLeftBlock
