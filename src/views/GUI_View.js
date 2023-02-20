b03ffca7464b42004700445edd775db2296d322f
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-02-19 23:50:32 -0500

    Added new buttons

--- src/views/GUI_View.js
+++ src/views/GUI_View.js
@@ -1,4 +1,25 @@
 //TODO!
 //Function to find a specifc html tasks by adding an ID for each tasks
 
-this.TopLeftBlock = document.getElementById(""); //Return the ID of element as a java script object, store all in the array and suffle and change what they say inside them This.TopLeftBlock
+//Return the ID of element as a java script object, store all in the array and suffle and change what they say inside them This.TopLeftBlock
+
+let MiddleLeftBlock = document.getElementById("MiddleLeftBlock");
+MiddleLeftBlock.innerHTML = "O";
+
+let TopLeftBlock = document.getElementById("TopLeftBlock");
+TopLeftBlock.innerHTML = "O";
+
+let Middle = document.getElementById("Middle");
+Middle.innerHTML = "O";
+
+let TopRightBlock = document.getElementById("TopRightBlock");
+TopRightBlock.innerHTML = "O";
+
+let BottomRightBlock = document.getElementById("BottomRightBlock");
+BottomRightBlock.innerHTML = "O";
+
+let BottomLeftBlock = document.getElementById("BottomLeftBlock");
+BottomLeftBlock.innerHTML = "O";
+
+let MiddleRightBlock = document.getElementById("MiddleRightBlock");
+MiddleRightBlock.innerHTML = "O";
