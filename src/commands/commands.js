289ae77702ee5a3bb30985c89dc02dad595cbaac
Mitchell Harrison <mitchellharrison42@gmail.com>
2023-04-16 22:01:03 -0400

    Appened to dictonary

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -3,6 +3,6 @@ const guess = require("./guessCommands.js");
 const { generateHint, generateTwoLetterHint } = require("./hintCommands.js");
 const { newPuzzle, calculatePoints, newPuzzleFromBase } = require("./newPuzzleCommands.js");
 const shuffle = require("./shuffleCommands.js");
-const { highScoreCommand } = require("./highScoreCommand.js");
+const { highScoreCommand, addHighScore } = require("./highScoreCommand.js");
 
-module.exports = { load, save, promptSave, guess, generateHint, generateTwoLetterHint, newPuzzle, calculatePoints,newPuzzleFromBase, highScoreCommand, shuffle };
+module.exports = { load, save, promptSave, guess, generateHint, generateTwoLetterHint, newPuzzle, calculatePoints,newPuzzleFromBase, highScoreCommand, addHighScore, shuffle };
--- src/commands/highScoreCommand.js
+++ src/commands/highScoreCommand.js
@@ -3,7 +3,6 @@ const prompt = require("prompt-sync")();
 const fileSystem = require("./fileSystem.js");
 
 // //TODO!!! STILL IN PROGRESS (fix the method @)
-// //TODO!! Insert into puzzle alphabetical
 // !!!NOTE: NEED TO CLEAR THE highScoreDict.json before client demo
 function highScoreCommand(Model) {
   //check if a puzzle is open
@@ -21,14 +20,6 @@ function highScoreCommand(Model) {
     .join()
     .replace(/,/g, "")
     .toUpperCase();
-
-  //check if the puzzle exists in the high score file
-  //if it doesnt exist the we should create a high score entry for it with the center letter
-  //else if it does exist then we should check that a high score entry for it with the center letter exists
-  //if the high score entry for it with the center letter doesnt exist then we should create it
-  //else if it does exist
-  //if the user has a highscore that can be put on the leader board then we should ask them for a user id and then put their score on the leader board
-  //else then we should print the high scores
 
   //check if the puzzle exists in the high score file
   if (!file.highscores.hasOwnProperty(letters)) {
@@ -55,4 +46,66 @@ function highScoreCommand(Model) {
   }
 }
 
-module.exports = { highScoreCommand };
+function addHighScore(Model) {
+  //check if a puzzle is open
+  if (!Model.isPuzzleOpen) {
+    console.log("SpellingBee> No puzzle open, no high-scores available");
+    return false;
+  }
+
+  //the current puzzle string in alphabetical order
+  let puzzle = Model.currentPuzzle
+    .sort()
+    .join()
+    .replace(/,/g, "")
+    .toUpperCase();
+
+  // Read the highscore file
+  let data = fs.readFileSync("highScoreDict.json");
+  let highscores = JSON.parse(data);
+
+  // Check if the puzzle exists in the highscore file
+  if (!highscores.highscores.hasOwnProperty(puzzle)) {
+    // If it doesn't, create a new entry for the puzzle
+    highscores.highscores[puzzle] = {
+      center_letter: Model.requiredLetter,
+      scores: [],
+    };
+  }
+
+  // Check if the center letter for the puzzle matches the required letter
+  if (highscores.highscores[puzzle].center_letter !== Model.requiredLetter) {
+    console.log("Center letter doesn't match the required letter");
+    return false;
+  }
+
+  // Create a new highscore object with the user ID and score
+  let newHighScore = {
+    user_id: "Jon",
+    score: Model.userPoints,
+  };
+
+  // Add the new highscore object to the "scores" array for the puzzle
+  let scores = highscores.highscores[puzzle].scores;
+  let index = scores.findIndex((s) => s.score < Model.userPoints);
+  if (index === -1) {
+    scores.push(newHighScore);
+  } else {
+    scores.splice(index, 0, newHighScore);
+  }
+  scores.splice(5);
+
+  // Write the updated JSON object to the file
+  fs.writeFileSync("highScoreDict.json", JSON.stringify(highscores, null, 2));
+
+  return true;
+
+  //if it doesnt exist the we should create a high score entry for it with the center letter
+  //else if it does exist then we should check that a high score entry for it with the center letter exists
+  //if the high score entry for it with the center letter doesnt exist then we should create it
+  //else if it does exist
+  //if the user has a highscore that can be put on the leader board then we should ask them for a user id and then put their score on the leader board
+  //else then we should print the high scores
+}
+
+module.exports = { highScoreCommand, addHighScore};
